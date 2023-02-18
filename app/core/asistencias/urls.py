835fc16f9d1dfe547c88b7ce24aa0337de27dd78
sergiojg19 <sergio.jumbo@unl.edu.ec>
2023-02-18 18:01:31 -0500

    mejorar estilos

--- app/core/asistencias/urls.py
+++ app/core/asistencias/urls.py
@@ -1,9 +1,10 @@
 
 from django.urls import path
-from core.asistencias.views.reporte.view import ReporteListView, ReporteCreateView, ReporteUpdateView
+from core.asistencias.views.reporte.view import ReporteListView, ReporteCreateView, ReporteUpdateView, ReporteDeleteView
 
 urlpatterns = [
     path('student/report/', ReporteListView.as_view(), name= 'reporteList'),
     path('student/create/', ReporteCreateView.as_view(), name= 'reporteCreate'),
-    path('student/edit/<int:pk>/', ReporteUpdateView.as_view(), name= 'reporteUpdate')
+    path('student/edit/<int:pk>/', ReporteUpdateView.as_view(), name= 'reporteUpdate'),
+    path('student/delete/<int:pk>/', ReporteDeleteView.as_view(), name='reporteDelete')
 ]
--- app/core/asistencias/views/reporte/view.py
+++ app/core/asistencias/views/reporte/view.py
@@ -2,7 +2,7 @@ from django.http import JsonResponse
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.decorators.csrf import csrf_exempt
-from django.views.generic import ListView, CreateView, UpdateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.utils.decorators import method_decorator
 
 from core.asistencias.forms import RegistroAsistenciaForm
@@ -65,6 +65,19 @@ class ReporteUpdateView(UpdateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Editar Asistencia'
+        context['entity'] = 'Asistencias'
+        context['list_url'] = reverse_lazy('reporteList')
+        context['action'] = 'edit'
+        return context
+
+class ReporteDeleteView(DeleteView):
+    model = RegistroAsistencia
+    template_name ='reporte/delete.html'
+    success_url =reverse_lazy('reporteList')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Eliminar Asistencia'
         context['entity'] = 'Asistencias'
         context['list_url'] = reverse_lazy('reporteList')
         context['action'] = 'edit'
