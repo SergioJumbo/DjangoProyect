841c6934e3604c07417a4e6eb84f8b6fab314650
sergiojg19 <sergio.jumbo@unl.edu.ec>
2023-02-18 16:32:04 -0500

    guardar y listar registro de asistencias

--- app/core/asistencias/forms.py
+++ app/core/asistencias/forms.py
@@ -1,9 +1,28 @@
-from django.forms import ModelForm
+from django.contrib.admin.widgets import AdminDateWidget
+from django.forms import *
 
 from core.asistencias.models import RegistroAsistencia
 
 
 class RegistroAsistenciaForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for form in self.visible_fields():
+            form.field.widget.attrs['class'] = 'form-control'
+            form.field.widget.attrs['autocomplete'] = 'off'
+
     class Meta:
         model = RegistroAsistencia
         fields = '__all__'
+        labels = {
+            'fecha':'Fecha de asistencia'
+        }
+        widgets = {
+            'fecha': DateInput(
+            attrs={
+                'type': 'date',
+                'placeholder': 'mm-dd-yyyy'
+                }
+            )
+
+        }
--- app/core/asistencias/views/reporte/view.py
+++ app/core/asistencias/views/reporte/view.py
@@ -1,5 +1,6 @@
 from django.http import JsonResponse
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import ListView, CreateView
 from django.utils.decorators import method_decorator
@@ -29,9 +30,6 @@ class ReporteListView(ListView):
         except Exception as e:
             data['error'] = str(e)
 
-        print(request.POST)
-        #print(regAsis)
-        #data['fecha'] = regAsis.fecha
         return JsonResponse(data)
     def get_queryset(self):
         return RegistroAsistencia.objects.all()
@@ -39,14 +37,21 @@ class ReporteListView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Reporte de Asistencias'
+        context['create_url'] = reverse_lazy('reporteCreate')
+        context['list_url'] = reverse_lazy('reporteList')
+        context['entity'] = 'Asistencias'
         return context
 
 class ReporteCreateView(CreateView):
     model= RegistroAsistencia
     form_class = RegistroAsistenciaForm
     template_name = 'reporte/create.html'
+    success_url = reverse_lazy('reporteList')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Registrar Asistencia'
+        context['entity'] = 'Asistencias'
+        context['list_url'] = reverse_lazy('reporteList')
         return context
+
